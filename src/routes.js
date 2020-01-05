7d2d179900dfe3b6c282725cbf9f872e1de49dce
Dailton Bastos <dailtonbastos@gmail.com>
2020-01-05 12:34:25 -0300

    Autenticação JWT

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,12 @@ const express = require('express');
 const routes = express.Router();
 
 const UserController = require('./app/controllers/UserController');
+const SessionController = require('./app/controllers/SessionController');
 
 // Users
 routes.post('/users', UserController.store);
+
+// Session
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
