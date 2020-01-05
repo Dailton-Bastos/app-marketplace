9da55c77c3ce4e16a88eddb8348a2d183a700006
Dailton Bastos <dailtonbastos@gmail.com>
2020-01-05 11:46:58 -0300

    Cadastro de usuário e uso do Insomnia

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const routes = express.Router();
 
+const UserController = require('./app/controllers/UserController');
+
+// Users
+routes.post('/users', UserController.store);
+
 module.exports = routes;
